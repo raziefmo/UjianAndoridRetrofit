02bcbce4fbca2649ad22e5088cccd25ad3df4f14
raziefmo <raziefmo@gmail.com>
2022-04-22 21:16:10 +0700

    addkedua

--- app/src/main/java/com/adl/ujianandoridretrofit/CheckinActivity.kt
+++ app/src/main/java/com/adl/ujianandoridretrofit/CheckinActivity.kt
@@ -1,17 +1,58 @@
 package com.adl.ujianandoridretrofit
 
+import android.app.Activity
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.github.drjacky.imagepicker.ImagePicker
+import kotlinx.android.synthetic.main.activity_checkin.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class CheckinActivity : AppCompatActivity() {
+
+    lateinit var photoURI: Uri
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val uri = it.data?.data!!
+
+                photo.setImageURI(uri)
+                photoURI = uri
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkin)
 
+        btnCamera.setOnClickListener({
+            cameraLauncher.launch(
+                ImagePicker.with(this)
+                    .crop()
+                    .cameraOnly()
+                    .maxResultSize(480, 800, true)
+                    .createIntent()
+            )
+
+        })
+
+        fun createRB(data: String): RequestBody {
+            return RequestBody.create(MultipartBody.FORM, data)
+        }
+
+        fun uploadImage(uri: Uri, param: String): MultipartBody.Part {
+            val file: File = File(uri.path)
+            val rb: RequestBody = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+
+            return MultipartBody.Part.createFormData(param, file.name, rb)
 
 
-
-
-
+        }
     }
 }
--- app/src/main/java/com/adl/ujianandoridretrofit/servis/Login.kt
+++ app/src/main/java/com/adl/ujianandoridretrofit/servis/Login.kt
@@ -1,6 +1,8 @@
 package com.adl.ujianandoridretrofit.servis
 
 import com.adl.ujianandoridretrofit.model.ResponsePostData
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -14,4 +16,12 @@ interface Login {
     @Headers("X-Api-Key:C1B5A29AB04F4C1C5AAE44AFF305ECA5")
     @POST("api/ujian/add")
     fun addData(@Field("username") username:String, @Field("password")password:String): Call<ResponsePostData>
+
+    @Multipart
+    @Headers("X-Api-Key:C1B5A29AB04F4C1C5AAE44AFF305ECA5")
+    @POST("api/ujian/add")
+    fun addDataAndImage(@Part file: MultipartBody.Part,
+                        @Part("dari") login: RequestBody,
+                        @Part("sampai") logout: RequestBody
+                        ):Call<ResponsePostData>
 }
